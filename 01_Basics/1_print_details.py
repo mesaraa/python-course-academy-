daa142f36402b7171e7b4a3a65c326b4631416d1
mesaraa <shaikhmuhmmadhamdan@gmail.com>
2025-11-15 19:25:34 +0530

    Solved - Done

--- 01_Basics/1_print_details.py
+++ 01_Basics/1_print_details.py
@@ -1,3 +1,7 @@
 # Ask user for name, age, and city
-
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
 # Print the details in one sentence
+print("Hello", name + ", you are", age, "years old and you live in", city + ".")
+
