c601a1b811997833631c7626f71e05337a90c9a1
mesaraa <shaikhmuhmmadhamdan@gmail.com>
2025-11-15 19:34:54 +0530

    Solved - Done

--- 01_Basics/3_area_of_rect.py
+++ 01_Basics/3_area_of_rect.py
@@ -1,5 +1,9 @@
 # Take length and width as input
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
 
 # Calculate area
+area = length * width
 
 # Print the result
+print("The area is:", area)
