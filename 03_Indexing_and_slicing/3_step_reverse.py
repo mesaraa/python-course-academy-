c10ee3baaed7731e3916daad47533a672de46c6b
mesaraa <shaikhmuhmmadhamdan@gmail.com>
2025-11-29 19:03:28 +0530

    Solved - Done

--- 03_Indexing_and_slicing/3_step_reverse.py
+++ 03_Indexing_and_slicing/3_step_reverse.py
@@ -1,4 +1,11 @@
 # Input a string
+s = input("Enter a string: ")
+
 # Print the reversed string using slicing
+print("Reversed string:", s[::-1])
+
 # Print every second character using slicing (step = 2)
+print("Every second character:", s[::2])
+
 # Print characters at even indexes using slicing
+print("Characters at even indexes:", s[0::2])
--- 03_Indexing_and_slicing/4_challenge.py
+++ 03_Indexing_and_slicing/4_challenge.py
@@ -1,4 +1,12 @@
 # Input an email address (like "student123@gmail.com")
+email = input("Enter your email: ")
+
 # Slice out the username (everything before '@')
+username = email.split("@")[0]
+
 # Remove the first and last character from that username
+trimmed_username = username[1:-1]
+
 # Print both: original username and trimmed username
+print("Original username:", username)
+print("Trimmed username:", trimmed_username)
